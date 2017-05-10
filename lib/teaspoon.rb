1cfe5abd6e46543c102a508e3866348e76bd7dd8
Daniel Giralt <danielgiralt@Daniel-Giralts-MB-Pro.local>
2017-05-10 15:00:38 +0200

    fix: changed code to adapt to tests

--- lib/teaspoon.rb
+++ lib/teaspoon.rb
@@ -5,7 +5,7 @@ require 'teaspoon/data_parser'
 
 module Teaspoon
   def self.measure(file_path, branch_name)
-    epoch = DataParser.epoch
+    epoch = Time.now.to_i
     statuses = DataParser.statuses(file_path)
     @conn = ConnectionFactory.create
     @conn.save(statuses, branch_name, epoch)
--- lib/teaspoon/data_parser.rb
+++ lib/teaspoon/data_parser.rb
@@ -1,25 +1,14 @@
 require 'json'
 
 module DataParser
-  def self.epoch
-    Time.now.to_i
-  end
-
   def self.statuses(file_path)
     s = StatusParser.new
-    json = read_execution(file_path)
+    json = s.read_execution(file_path)
     out = []
     json.each do |feature|
       feature[:elements].each { |scenario| out.push(s.status(scenario)) }
     end
     out
-  end
-
-  def self.read_execution(file_path)
-    report = File.open(file_path)
-    data = report.read
-    report.close
-    JSON.parse(data, symbolize_names: true)
   end
 
   class StatusParser
@@ -31,6 +20,13 @@ module DataParser
               scenario[:after].to_a
       steps.each { |step| pass &&= step[:result][:status].eql?('passed') }
       { name: name, status: pass }
+    end
+
+    def read_execution(file_path)
+      report = File.open(file_path)
+      data = report.read
+      report.close
+      JSON.parse(data, symbolize_names: true)
     end
   end
 end
