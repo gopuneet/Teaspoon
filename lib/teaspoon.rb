c6fa22b8730996b2ab3be78c61e9a7fe1829e97c
Daniel Giralt <danielgiralt@Daniel-Giralts-MB-Pro.local>
2017-05-19 13:03:17 +0200

    Extended interface

--- lib/teaspoon.rb
+++ lib/teaspoon.rb
@@ -5,12 +5,20 @@ require 'teaspoon/connections/connection_factory'
 require 'teaspoon/data_parser'
 
 module Teaspoon
-  def self.pour(file_path, branch_name = 'master')
+  def self.measure(file_path)
+    DataParser.statuses(file_path)
+  end
+
+  def self.pour(input, branch_name = 'master')
     epoch = Tea.time
-    statuses = DataParser.statuses(file_path)
     @conn = ConnectionFactory.create
-    @conn.save(statuses, branch_name, epoch)
+    @conn.save(input, branch_name, epoch)
     @conn.close
+  end
+
+  def self.measure_and_pour(file_path, branch_name = 'master')
+    input = measure(file_path)
+    pour(input, branch_name)
   end
 
   def self.spoonful(constraints = {})
