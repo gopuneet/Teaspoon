bb395e6d6392036aa7a17e582842b69cde15f581
Daniel Giralt <danielgiralt@Daniel-Giralts-MB-Pro.local>
2018-03-16 15:54:34 +0100

    firebase to retrieve and save data

--- lib/teaspoon/connections/firebase_connection.rb
+++ lib/teaspoon/connections/firebase_connection.rb
@@ -2,14 +2,19 @@ require 'teaspoon/connections/db_connection'
 require 'firebase'
 
 class FireBaseConnection < DBConnection
+  @@PATH = 'statuses'
+
   def initialize(data)
-    private_key = File.read(data.fetch[:private_key_path])
+    private_key = File.read(data.fetch(:private_key_path))
     @db = Firebase::Client.new(data.fetch(:url), private_key)
     super
   end
 
   def save(statuses, branch_name, timestamp)
     super
+    constant_data = {branch: branch_name, epoch: timestamp}
+    data = statuses.map { |status| status.merge(constant_data) }
+    @db.push(@@PATH, data)
   end
 
   def close
@@ -18,15 +23,26 @@ class FireBaseConnection < DBConnection
 
   private
 
-  def configure
-
-  end
-
   def data(constraints = {})
-
+    raw = @db.get(@@PATH).body
+    treated = raw.values.flatten.map do |scenario|
+      symbolize_keys(scenario).tap do |scenario|
+        scenario[:scenario] = scenario[:name]
+        scenario.delete(:name)
+      end
+    end
+    treated.select do |scenario|
+      @@id_keys.map do |key|
+        constraints[key] ? constraints[key].include?(scenario[key]) : true
+      end.all?
+    end
   end
 
   def ids(key)
 
+  end
+
+  def symbolize_keys(hash)
+    Hash[hash.map { |k,v| [k.to_sym, v] } ]
   end
 end
--- teaspoon.gemspec
+++ teaspoon.gemspec
@@ -35,6 +35,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency 'bundler', '~> 1.14'
   spec.add_dependency 'dotenv', '~> 2.2.1'
+  spec.add_dependency 'firebase'
   spec.add_dependency 'mysql'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_dependency 'redis', '~> 3.2'
